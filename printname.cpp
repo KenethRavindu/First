642285b4246ec7bd809123524b6ae2f90021a03e
Ken31h <kenethravindu2000@gmail.com>
2024-06-09 14:57:35 +0530

    Printing Your name

---
+++ printname.cpp
@@ -0,0 +1,14 @@
+//Printing Your name
+#include<iostream>
+using namespace std;
+int main(){
+
+    string name;
+
+    cout<<"Enter your name: ";
+    cin>>name;
+
+    cout<<"The name you entered is: "<<name<<endl;
+
+    return 0;
+}
