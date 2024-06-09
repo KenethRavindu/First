870cf32325dbe4fa6033dc94d2e08633ac8d2f3b
Ken31h <kenethravindu2000@gmail.com>
2024-06-09 14:56:32 +0530

    C++ program to get an input and output a number

---
+++ inputanumber.cpp
@@ -0,0 +1,15 @@
+//C++ program to get an input and output a number
+#include<iostream>
+using namespace std;
+int main(){
+
+    int num;
+    cout<<"Enter a number: ";
+
+    cin>>num;
+
+    cout<<"Number you Entered: "<<num;
+
+    return 0;
+    
+}
