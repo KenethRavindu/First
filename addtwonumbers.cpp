9133a17b9fec8958211bd8484c29889d5d207f60
Ken31h <kenethravindu2000@gmail.com>
2024-06-09 14:59:30 +0530

    C++ program to add 2 numbers

---
+++ addtwonumbers.cpp
@@ -0,0 +1,17 @@
+//C++ program to add 2 numbers
+#include<iostream>
+using namespace std;
+int main(){
+
+    int sum, num1, num2;
+    cout<<"Enter number 1: ";
+    cin>>num1;
+    cout<<"Enter number 2: ";
+    cin>>num2;
+
+    sum = num1 + num2;
+
+    cout<<"The Addition of 2 numbers are: "<<sum<<endl;
+
+    return 0;
+}
